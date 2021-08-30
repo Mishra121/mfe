6d7a5698ca0f3983e57e704ed7f808d796684d3e
Vibhu Mishra <vibhumishra@Vibhus-MacBook-Pro.local>
2021-08-30 17:55:25 +0530

    routing logic for container and child mfe

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App'
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+
+    const history = defaultHistory || createMemoryHistory();
+
+    if(onNavigate) {
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     );
+
+    return {
+        onParentNavigate({ pathname: nextPathname}) {
+
+            const { pathname } = history.location;
+
+            if(pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 };
 
 // If we are in development and in isolation,
@@ -16,7 +35,7 @@ if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if(devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
