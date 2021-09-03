e25f2f837bcea02d75402d7902a869124e0288a6
Vibhu Mishra <vibhumishra@Vibhus-MacBook-Pro.local>
2021-09-04 03:20:18 +0530

    Auth sharing funda

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
 import Header from './components/Header'
+import Progress from './components/Progress';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
 })
 
-export default () => (
-    <StylesProvider generateClassName={generateClassName}>
-    <BrowserRouter>
-    <div>
-        <Header />
-        <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-        </Switch>
-        Hi There ! Learning Micro Forntends
-    </div>
-    </BrowserRouter>
-    </StylesProvider>
-);
+export default () => {
+
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    return (
+        <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+        <div>
+            <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+            <Suspense fallback={<Progress />}>
+            <Switch>
+                <Route path="/auth">
+                    <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                </Route>
+                <Route path="/" component={MarketingLazy} />
+            </Switch>
+            </Suspense>
+            Hi There ! Learning Micro Forntends
+        </div>
+        </BrowserRouter>
+        </StylesProvider>
+    )
+}
